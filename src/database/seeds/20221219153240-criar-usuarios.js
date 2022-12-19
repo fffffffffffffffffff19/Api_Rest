4f1cf40d0ae662db5b7894c388f76a34d2bb777c
-ffffffffff <davischiavon21@gmail.com>
2022-12-19 12:42:51 -0300

    Creating seeds

---
+++ src/database/seeds/20221219153240-criar-usuarios.js
@@ -0,0 +1,35 @@
+const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          nome: '1Schiavon',
+          email: '1sim@gmail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: '2Schiavon',
+          email: '2sim@gmail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: '3Schiavon',
+          email: '3sim@gmail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {},
+    );
+  },
+
+  down: () => { },
+};
