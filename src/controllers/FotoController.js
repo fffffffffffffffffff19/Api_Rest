a2983bfa93f71540959d48305efa3583119c48b5
fffffffffffffffffff19 <davischiavon21@gmail.com>
2022-12-27 14:47:39 -0300

    tratando midia

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,16 +1,21 @@
 import multer from 'multer';
 import multerConfig from '../config/multer';
 
+import Foto from '../models/Foto';
+
 const upload = multer(multerConfig).single('foto');
 
 class FotoController {
-  async create(req, res) {
-    return upload(req, res, (error) => {
+  create(req, res) {
+    return upload(req, res, async (error) => {
       if(error) {
         return res.status(400).json({
           errors: [error],
         });
       }
+
+      const { originalname, filename } = req.file;
+      const foto = await Foto.create({ originalname, filename });
 
       return res.json(req.file);
     })
--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import loginRequired from '../middlewares/loginRequired';
 
 import fotoController from '../controllers/FotoController';
 
 
 const router = new Router();
 
-router.post('/', fotoController.create);
+router.post('/', loginRequired, fotoController.create);
 
 export default router;
