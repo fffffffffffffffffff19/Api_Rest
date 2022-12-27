cd92a7b80f0d79618c87dec71e44814f6ed8e128
fffffffffffffffffff19 <davischiavon21@gmail.com>
2022-12-27 14:01:49 -0300

    sim

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,6 +1,19 @@
+import multer from 'multer';
+import multerConfig from '../config/multer';
+
+const upload = multer(multerConfig).single('foto');
+
 class FotoController {
   async create(req, res) {
-    res.json(req.file);
+    return upload(req, res, (error) => {
+      if(error) {
+        return res.status(400).json({
+          errors: [error],
+        });
+      }
+
+      return res.json(req.file);
+    })
   }
 }
 
